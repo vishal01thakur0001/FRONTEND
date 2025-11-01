645f6110bd6097b1cdda59c2bfc5355b93c3aeba
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2025-11-01 14:45:48 +0530

    commit

---
+++ JavaScrip/Array/Array.js
@@ -0,0 +1,17 @@
+// const a = new Array(8);
+// console.log(a);
+// a[9] = 10;
+// console.log(a);
+
+// const a = [1, 2, 3, 4, 5];
+// console.log(a.length);
+
+// const a = [1, 2, 3, 4, 5];
+// const lastValue = a.pop();
+// console.log(lastValue);
+// console.log(a);
+
+const a = [1, 2, 3, 4, 5];
+a.unshift(0);
+a.shift();
+console.log(a);
---
+++ JavaScrip/Basic Js/scrpt.js
@@ -0,0 +1,9 @@
+// console.log("Vishal");
+// let a = 5;
+// let b = 6;
+// console.log(a + b);
+// console.log("Hello World");
+const a = new Array(8);
+console.log(a);
+a[9] = 10;
+console.log(a);
