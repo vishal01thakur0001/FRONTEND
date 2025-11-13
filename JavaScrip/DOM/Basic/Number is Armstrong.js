6485438f64175b9db2a4b1ae8737cbe38bc76eb8
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2025-11-13 22:02:58 +0530

    commit

---
+++ JavaScrip/DOM/Basic/Number is Armstrong.js
@@ -0,0 +1,10 @@
+let num = 153;
+let sum = 0;
+let temp = num;
+
+while (temp > 0) {
+    let digit = temp % 10;
+    sum += digit ** 3;
+    temp = Math.floor(temp / 10);
+}
+console.log(sum === num ? "Armstrong" : "Not Armstrong");
